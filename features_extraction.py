d14e4f18091e75282ba52f9ea70b3e98487bad8a
DelannoyArthur <delannoyarthur@outlook.com>
2026-07-02 16:59:09 +0200

    compute nb zones and small improvements

--- features_extraction.py
+++ features_extraction.py
@@ -1,6 +1,6 @@
 import math
 import os
-# TODO: ExifTags?
+# TODO: ExifTags? (also comments in the main)
 from PIL import Image, ExifTags
 from rembg import remove
 
@@ -117,6 +117,17 @@ def get_hue_pixel(image, x, y):
     return pixel_hue
 
 
+def get_hue_histogram(image):
+    width, height = image.size
+    hue_hist = [0]*360
+    for x in range(width):
+        for y in range(height):
+            pixel_hue = get_hue_pixel(image, x, y)
+            if pixel_hue != 360:
+                hue_hist[pixel_hue] += 1
+    return hue_hist
+
+
 def create_bin_mask(image):
     width, height = image.size
     bin_mask = Image.new("L", (width, height))
@@ -128,8 +139,85 @@ def create_bin_mask(image):
     return bin_mask
 
 
-def compute_nb_area():
-    pass
+def compute_nb_area(mask, test_view=False):
+    width, height = mask.size
+    bin_coordinate = [-1, -1, -1, -1]
+    x = 0
+    while x < width:
+        y = 0
+        while y < height:
+            current_pixel = mask.getpixel((x, y))
+            if current_pixel != 0:
+                if bin_coordinate[0] < 0:
+                    bin_coordinate[0] = x
+                    bin_coordinate[1] = y
+                    bin_coordinate[2] = x
+                    bin_coordinate[3] = y
+                elif bin_coordinate[1] > y:
+                    bin_coordinate[1] = y
+                elif bin_coordinate[2] < x:
+                    bin_coordinate[2] = x
+                elif bin_coordinate[3] < y:
+                    bin_coordinate[3] = y
+            y += 1
+        x += 1
+
+    approximate_zone_size = ((bin_coordinate[2] - bin_coordinate[0])/6, (bin_coordinate[3] - bin_coordinate[1])/6)
+    nb_zone = [0, 0]
+
+    divisors = [1, 1]
+    potential_zone_sizes = [width, width]
+    test_divisor = 0
+    while test_divisor <= math.floor(math.sqrt(width)) and potential_zone_sizes[1] > approximate_zone_size[0]:
+        test_divisor += 1
+        if width % test_divisor == 0:
+            divisors[0] = divisors[1]
+            divisors[1] = test_divisor
+            potential_zone_sizes[0] = potential_zone_sizes[1]
+            potential_zone_sizes[1] = width//divisors[1]
+
+    if abs(approximate_zone_size[0] - potential_zone_sizes[1]) > width//8 and abs(approximate_zone_size[0] - potential_zone_sizes[0]) > width//8:
+        nb_zone[0] = 8
+    elif potential_zone_sizes[1] >= approximate_zone_size[0] or approximate_zone_size[0] - potential_zone_sizes[1] <= potential_zone_sizes[0] - approximate_zone_size[0]:
+        nb_zone[0] = divisors[1]
+    else:
+        nb_zone[0] = divisors[0]
+
+    divisors = [1, 1]
+    potential_zone_sizes = [height, height]
+    test_divisor = 0
+    while test_divisor <= math.floor(math.sqrt(height)) and potential_zone_sizes[1] > approximate_zone_size[1]:
+        test_divisor += 1
+        if height % test_divisor == 0:
+            divisors[0] = divisors[1]
+            divisors[1] = test_divisor
+            potential_zone_sizes[0] = potential_zone_sizes[1]
+            potential_zone_sizes[1] = height//divisors[1]
+
+    if abs(approximate_zone_size[1] - potential_zone_sizes[1]) > height//8 and abs(approximate_zone_size[1] - potential_zone_sizes[0]) > height//8:
+        nb_zone[1] = 8
+    elif potential_zone_sizes[1] >= approximate_zone_size[1] or approximate_zone_size[1] - potential_zone_sizes[1] <= potential_zone_sizes[0] - approximate_zone_size[1]:
+        nb_zone[1] = divisors[1]
+    else:
+        nb_zone[1] = divisors[0]
+
+    if test_view is True:
+        print("approximate", approximate_zone_size)
+        print("nb zone:", nb_zone)
+        zone_size = (width//nb_zone[0], height//nb_zone[1])
+        print("zone size:", zone_size)
+        test_zones = Image.new("RGB", (width, height))
+        for x in range(width):
+            for y in range(height):
+                if (x % zone_size[0]) == zone_size[0] - 1 or (y % zone_size[1]) == zone_size[1] - 1:
+                    test_zones.putpixel((x, y), (255, 0, 255))
+                elif bin_coordinate[0] <= x <= bin_coordinate[2] and bin_coordinate[1] <= y <= bin_coordinate[3]:
+                    test_zones.putpixel((x, y), (255, 255, 255))
+                else:
+                    test_zones.putpixel((x, y), (0, 0, 0))
+        test_zones.show()
+
+    return nb_zone[0], nb_zone[1]
 
 
 def create_bin_image_old(image, bin_mask, nb_x_area, nb_y_area, area_border=False):  # not used
@@ -151,7 +239,7 @@ def create_bin_image_old(image, bin_mask, nb_x_area, nb_y_area, area_border=Fals
             if area_value >= (area_width*area_height)/8 or above_area_value >= (area_width*area_height)/4:
                 for x in range(area_width):
                     for y in range(area_height):
-                        if area_border is True and (y == area_height - 1 or x == area_width - 1):
+                        if area_border is True and (x == area_width - 1 or y == area_height - 1):
                             bin_image.putpixel((area_width*area_x + x, area_height * area_y + y), (255, 0, 255, 255))
                         else:
                             bin_image.putpixel((area_width*area_x + x, area_height*area_y + y), image.getpixel((area_width*area_x + x, area_height*area_y + y)))
@@ -257,7 +345,7 @@ def create_bin_image(image, contour_matrix, nb_x_area, nb_y_area, area_border=Fa
             if contour_matrix[area_x][area_y] > 0:
                 for x in range(area_width):
                     for y in range(area_height):
-                        if area_border is True and (y == area_height - 1 or x == area_width - 1):
+                        if area_border is True and (x == area_width - 1 or y == area_height - 1):
                             bin_image.putpixel((area_width*area_x + x, area_height * area_y + y), (255, 0, 255, 255))
                         else:
                             bin_image.putpixel((area_width*area_x + x, area_height*area_y + y), image.getpixel((area_width*area_x + x, area_height*area_y + y)))
@@ -265,66 +353,66 @@ def create_bin_image(image, contour_matrix, nb_x_area, nb_y_area, area_border=Fa
     return bin_image
 
 
-def get_hue_histogram(image):
-    width, height = image.size
-    hue_hist = [0]*360
-    for x in range(width):
-        for y in range(height):
-            pixel_hue = get_hue_pixel(image, x, y)
-            if pixel_hue != 360:
-                hue_hist[pixel_hue] += 1
-    return hue_hist
-
-
 if __name__ == '__main__':
     file_size = get_file_size(default_image_path)
-    print(file_size)
+    print("file size:", file_size)
+
     img = get_image(default_image_path)
     img.show()
+
     #exif = {ExifTags.TAGS[k]: v for k, v in img.getexif().items() if k in ExifTags.TAGS}
     #print(exif)
+
     img_size = get_image_size(img)
-    print(img_size)
+    print("image size:", img_size)
 
     avg_r, avg_g, avg_b, (min_r, max_r), (min_g, max_g), (min_b, max_b) = get_image_color_stats(img)
-    print((avg_r, avg_g, avg_b))
-    print(min_r, max_r)
-    print(min_g, max_g)
-    print(min_b, max_b)
+    print("average RGB:", (avg_r, avg_g, avg_b))
+    print("min/max R:", min_r, max_r)
+    print("min/max G:", min_g, max_g)
+    print("min/max B:", min_b, max_b)
 
     img_l = get_image_l(img)
     #img_l.show()
 
     avg_l, (min_l, max_l) = get_image_l_stats(img_l)
-    print(avg_l)
-    print(min_l, max_l)
+    print("average L:", avg_l)
+    print("min/max L:", min_l, max_l)
 
-    img_histogram = get_image_histogram(img_l)
-    print(img_histogram)
+    img_histogram = get_image_histogram(img)
+    print("histogram RGB:", img_histogram)
+
+    img_l_histogram = get_image_histogram(img_l)
+    print("histogram L:", img_l_histogram)
 
     contrast = get_image_l_contrast(max_l, min_l)
-    print(contrast)
+    print("contrast:", contrast)
+
+    hue_histogram = get_hue_histogram(img)
+    print("histogram hue:", hue_histogram)
 
     img_no_bg = remove(img)
-    img_no_bg.show()
+    #img_no_bg.show()
 
     msk_bin = create_bin_mask(img_no_bg)
     msk_bin.show()
 
-    bin_matrix = create_contour_matrix(img, msk_bin, 8, 8)
-    print(bin_matrix)
+    nb_area = compute_nb_area(msk_bin, test_view=True)
+    print("nb area:", nb_area)
 
-    img_bin = create_bin_image(img, bin_matrix, 8, 8)
+    bin_matrix = create_contour_matrix(img, msk_bin, nb_area[0], nb_area[1])
+    print("bin matrix:", bin_matrix)
+
+    img_bin = create_bin_image(img, bin_matrix, nb_area[0], nb_area[1])
     img_bin.show()
 
     img_bin_histogram = get_image_histogram(img_bin)
-    print(img_bin_histogram)
-    print(img_bin_histogram[0:256], img_bin_histogram[512:768])
-    print(sum(img_bin_histogram[0:256]))
+    print("nb pixel bin:", sum(img_bin_histogram[0:256]))
+    print("histogram bin:", img_bin_histogram)
+    print("histogram bin (no G):", img_bin_histogram[0:256], img_bin_histogram[512:768])
 
-    hue_histogram = get_hue_histogram(img_bin)
-    print(hue_histogram)
-    print(sum(hue_histogram))
+    hue_histogram_bin = get_hue_histogram(img_bin)
+    print("histogram hue bin:", hue_histogram_bin)
 
     # test contour matrix repartition
     #test_contour_matrix()
